383630e48646a2aeb54d595cc3587f9057b2b864
Nicolai Henriksen <nih@miracle.dk>
2016-08-05 12:50:53 +0200

    Added unit test project

---
+++ XamBootCamp.UTest/FirstViewModelTest.cs
@@ -0,0 +1,41 @@
+﻿using MvvmCross.Test.Core;
+using NUnit.Framework;
+using XamBootCamp.ViewModels;
+
+namespace XamBootCamp.UTest
+{
+    [TestFixture]
+    public class FirstViewModelTest : MvxIoCSupportingTest
+    {
+        public FirstViewModelTest ()
+        {
+            base.Setup ();
+        }
+
+        protected override void AdditionalSetup ()
+        {
+            // an automatically Mocked service:
+            // var firstService = new Mock<IFirstService> ();
+            // Ioc.RegisterSingleton<IFirstService> (firstService.Object);
+        }
+
+        protected const string _dontPanic = "Don't Panic!";
+
+        [Test]
+        public void TestHello ()
+        {
+            var vm = new FirstViewModel ();
+            vm.Hello = _dontPanic;
+            Assert.AreEqual (_dontPanic, vm.Hello);
+        }
+
+        [Test]
+        public void TestInitial ()
+        {
+            var vm = new FirstViewModel ();
+
+            Assert.AreEqual ("Hello MvvmCross", vm.Hello);
+        }
+    }
+}
+
